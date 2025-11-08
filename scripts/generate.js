98b9905bde304e6f818a7f95f92fc9ba542e9e12
Joham <jklarenbeek@gmail.com>
2025-11-08 18:03:56 +0100

    added a command line utility for showing the generated patterns.

---
+++ scripts/generate.js
@@ -0,0 +1,47 @@
+// scripts/generate.js
+/* eslint-env node */
+
+import {
+  generateAminoAcidInfo,
+  generateSixiangInfo,
+  generateTransitionInfo,
+  generateSymmetryInfo,
+  generateMantraInfo,
+  generateTaoInfo,
+  generateTrigramInfo,
+  getHexagramSequences
+} from '../src/utils/tools.js';
+
+// eslint-disable-next-line no-undef
+const command = process.argv[2];
+
+const functions = {
+  amino: generateAminoAcidInfo,
+  sixiang: generateSixiangInfo,
+  transition: generateTransitionInfo,
+  symmetry: generateSymmetryInfo,
+  mantra: generateMantraInfo,
+  tao: generateTaoInfo,
+  trigram: generateTrigramInfo,
+  sequences: getHexagramSequences,
+  all: () => ({
+    aminoAcidInfo: generateAminoAcidInfo(),
+    sixiangInfo: generateSixiangInfo(),
+    transitionInfo: generateTransitionInfo(),
+    symmetryInfo: generateSymmetryInfo(),
+    mantraInfo: generateMantraInfo(),
+    taoInfo: generateTaoInfo(),
+    trigramInfo: generateTrigramInfo(),
+    hexagramSequences: getHexagramSequences()
+  })
+};
+
+if (!command || !functions[command]) {
+  console.error('Usage: node generate.js <command>');
+  console.error('Available commands: amino, sixiang, transition, symmetry, mantra, tao, trigram, sequences, all');
+  // eslint-disable-next-line no-undef
+  process.exit(1);
+}
+
+const result = functions[command]();
+console.log(JSON.stringify(result, null, 2));
